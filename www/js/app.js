249c660db38fdde0f1a2bc9cce5cc2737c5a5bfd
yuliamurziak <murzia4ka@gmail.com>
2018-02-13 17:57:31 -0500

    camera

--- www/js/app.js
+++ www/js/app.js
@@ -1,16 +1,26 @@
 // Ionic Starter App
 
+// device ready event 
+
+document.addEventListener("deviceready", function(){
+  alert(JSON.stringify(navigator.camera));
+}, false); // from hellophone ga
+
+
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic'])
 //added here a controller
 .controller("takePicture", function($scope, $http){
+  $scope.model={};
+  $scope.model.imageSource="img/ionic.png" ;
   $scope.takePhoto=function(){
     alert("Taking photo");
     // gets a pictire. 
     navigator.camera.getPicture(function(imageData){
       alert(imageData);
+      $scope.model.imageSource=imageData.imageSource;
     },
     function(message){
       console.log(message);
